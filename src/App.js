db036d4e6df9653c5d62267178271994c6a0ae45
brhanuhailu <hailubrhanu29@gmail.com>
2023-03-23 10:43:54 +0000

    app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import React, { PureComponent } from 'react';
 import Calculator from './components/Calculator';
+import Quotes from './components/quote';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="quote-container">
+        <div className="quote">
+          <Quotes />
+        </div>
+        <div className="cal-contanier">
+          <Calculator />
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
